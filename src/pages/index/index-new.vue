aa33f6236d389f041a280480742861b008e348de
wanglingsui <wanglingsui@gmail.com>
2018-07-17 16:41:43 +0800

    sef

---
+++ src/pages/index/index-new.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="index-bar">
+    <div class="top-box">
+      <div class="location">
+        <span class="cityname"></span>
+        <span class="iconfont icon-arrow-down"></span>
+      </div>
+      <div class="search-input">
+        <span class="iconfont icon-search"></span>
+        <span class="text">大家正在搜：{{searchHotWords}}</span>
+      </div>
+      <div class="sns" @tap="tosns">
+        <span class="iconfont icon-zixun"></span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      searchHotWords: 'test'
+
+    }
+  },
+  methods: {
+    tosns () {
+      console.log('sns')
+    }
+  }
+}
+</script>
+
+<style>
+.index-bar {
+  width: 100%;
+  height: 142px;
+  overflow: hidden;
+}
+</style>
+
+
